3238a08daa2c5223bb67db495d4efae733cef8e2
liuxu <liuxu@wxchina.com>
2023-12-27 20:21:22 +0800

    1.增加tokio依赖 2.增加json的序列化操作 3.实验访问http请求

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,54 @@
-use std::error::Error;
+use std::{error::Error, f32::consts::E, io, vec};
 
-use tokio::{io::AsyncWriteExt, net::TcpStream};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt, Interest},
+    net::TcpStream,
+};
 
 mod remoting;
 
 #[tokio::main]
-pub async fn main() -> Result<(), Box<dyn Error>> {
-    let mut stream = TcpStream::connect("172.16.1.133:9876").await?;
-    println!("Create tcp stream to RocketMQ nameserver...");
-    let result = stream.write_all(b"hello world").await;
-    println!("响应的结果:{:?}", result.is_ok());
+pub async fn main() {
+    remoting::broker_information_command();
+    let mut stream = TcpStream::connect("tekton.cpaas.wxchina.com:30669")
+        .await
+        .unwrap();
 
-    return Ok(());
+    loop {
+        let ready = stream
+            .ready(Interest::READABLE | Interest::WRITABLE)
+            .await
+            .unwrap();
+
+        if ready.is_readable() {
+            let mut data = vec![0; 1024];
+            match stream.try_read(&mut data) {
+                Ok(n) => {
+                    println!("read bytes count:{}", n);
+                    let response = String::from_utf8_lossy(&data).to_string();
+                    println!("从服务端接收到的:{}", response);
+                }
+                Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                    continue;
+                }
+                Err(e) => {
+                    return;
+                }
+            }
+        }
+
+        if ready.is_writable() {
+            match stream.try_write(b"GET / HTTP/1.1\r\nHost: tekton.cpaas.wxchina.com \r\n\r\n") {
+                Ok(n) => {
+                    println!("Write bytes count:{}!", n);
+                }
+                Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                    continue;
+                }
+                Err(e) => {
+                    return;
+                }
+            }
+        }
+    }
 }
--- src/remoting/mod.rs
+++ src/remoting/mod.rs
@@ -1,1 +1,8 @@
+use self::command::Command;
+
 pub mod command;
+
+pub fn broker_information_command() {
+    let command = Command::new(28, String::from(""));
+    command.encode();
+}
